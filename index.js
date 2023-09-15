721c45ff1f01482cbeefcf3060e484a60e99ce14
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2023-09-15 01:24:43 +0100

    Refactor backend code

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-const express = require("express");
-const app = express();
+// const server = require("./public/server/server");
+const { start } = require("./public/backend/server");
 
-let timestamp = 0;
-
-// Simple logger
-app.use((req, res, next) => {
-    if (req.path === "/") {
-        console.log("\n\nip: ", req.ip);
-    }
-    console.log(req.method, " ", req.path);
-
-    next();
-});
-
-app.use(express.static("public")); // Allows me to reference assets using /assets/x.png
-app.use(express.static("public/src")); // can reference files in /public/src directly
-
-app.get("/", (req, res) => {
-    res.send("error, this should be overridden...!");
-});
-
-app.get("/timestamp", (req, res) => {
-    timestamp++;
-    res.send(timestamp.toString());
-});
-
-app.listen(3000, () => {
-    console.log("Listening on port 3000...");
-});
+start();
---
+++ public/backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+let timestamp = 0;
+
+const start = () => {
+    // Simple logger
+    app.use((req, res, next) => {
+        if (req.path === "/") {
+            console.log("\n\nip: ", req.ip);
+        }
+        console.log(req.method, " ", req.path);
+
+        next();
+    });
+
+    app.use(express.static("public")); // Allows me to reference assets using /assets/x.png
+    app.use(express.static("public/src")); // can reference files in /public/src directly
+
+    app.get("/", (req, res) => {
+        res.send("error, this should be overridden...!");
+    });
+
+    app.get("/timestamp", (req, res) => {
+        timestamp++;
+        res.send(timestamp.toString());
+    });
+
+    app.listen(3000, () => {
+        console.log("Listening on port 3000...");
+    });
+};
+
+module.exports = { start };
