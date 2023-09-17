61d26e4b0fe966d41bdf9caa6b09399471438ece
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2023-09-17 19:09:01 +0100

    Use Router to handle timestamp route request

--- backend/routes/timestamp.js
+++ backend/routes/timestamp.js
@@ -1,8 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+router.use(express.json());
+
 let counter = 0;
+let seconds = 0;
 
 const timestamp = (req, res) => {
-    counter++;
-    res.send(counter.toString());
+    const file = fs.readFileSync(path.resolve(__dirname, "../video.json"));
+    res.type("json").send(file);
 };
 
-module.exports = timestamp;
+router.get("/", timestamp);
+router.post("/", (req, res) => {
+    console.log(req.body);
+
+    // Get value from request
+
+    // Convert to number
+
+    // Update seconds
+    res.end();
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,13 @@ const start = () => {
     app.use(logger); // Simple log method & path
     app.use(express.static("public")); // Allows me to reference assets using /assets/x.png
     app.use(express.static("public/src")); // can reference files in /public/src directly
+    app.use("/timestamp", timestamp); // use timestamp file to handle endpoints
 
     // Does nothing, default route is served through static middleware
     app.get("/", (req, res) => {
         res.send("error, this should be overridden...!");
     });
     app.get("/search", search);
-    app.get("/timestamp", timestamp);
 
     app.listen(3000, () => {
         console.log("Listening on port 3000...");
