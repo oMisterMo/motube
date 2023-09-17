c64086e5bd601048022f36e1754a31015d2ecbec
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2023-09-17 19:47:02 +0100

    Make post request when user searches for link

--- public/src/app.js
+++ public/src/app.js
@@ -1,22 +1,45 @@
 // Initial fetch
 $(async function () {
     console.log(" ready!");
-});
 
-// Set button listener const
-const search = document.querySelector("#search");
-const searchButton = document.querySelector("#searchConfirm");
-const value = document.querySelector("#timestamp");
+    // Set button listener const
+    const search = document.querySelector("#search");
+    const searchButton = document.querySelector("#searchConfirm");
+    const value = document.querySelector("#timestamp");
 
-searchButton.addEventListener("click", async e => {
-    e.preventDefault();
+    const timestamp = await fetch("/timestamp");
+    const timestampJSON = await timestamp.json();
+    console.log(timestampJSON);
 
-    const url = search.value;
-    const a = document.createElement("a");
-    if (url) {
-        a.href = "/search?v=" + url;
-    } else {
-        a.href = "/search";
-    }
-    a.click();
+    searchButton.addEventListener("click", async e => {
+        e.preventDefault();
+
+        // Get details
+        const url = search.value;
+
+        // Update server file storing details of request
+        const data = {
+            url,
+            timestamp: 0,
+            created_at: Date.now(),
+            modified_at: Date.now(),
+        };
+        fetch("/timestamp", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        // Go to search page
+        const a = document.createElement("a");
+        if (url) {
+            a.href = "/search?v=" + url;
+        } else {
+            a.href = "/search";
+        }
+        a.click();
+    });
 });
