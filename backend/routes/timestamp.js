c1f3050648332044aeee773822a714e45d065cb4
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2023-09-17 19:59:34 +0100

    Tidy up routes

--- backend/routes/timestamp.js
+++ backend/routes/timestamp.js
@@ -7,21 +7,36 @@ router.use(express.json());
 let counter = 0;
 let seconds = 0;
 
-const timestamp = (req, res) => {
-    const file = fs.readFileSync(path.resolve(__dirname, "../video.json"));
+// Get value from request
+// Convert to number
+// Update seconds
+
+const filePath = path.resolve(__dirname, "../video.json");
+
+const timestampGET = (req, res) => {
+    const file = fs.readFileSync(filePath);
     res.type("json").send(file);
 };
-
-router.get("/", timestamp);
-router.post("/", (req, res) => {
+const timestampPOST = (req, res) => {
     console.log(req.body);
+    const data = req.body;
+    console.log(data.url);
 
-    // Get value from request
+    if (data.url) {
+        try {
+            fs.writeFileSync(filePath, JSON.stringify(data));
+        } catch (err) {
+            res.status(500).send("Error writing file.");
+        }
+    }
+    res.status(201).end();
+};
 
-    // Convert to number
-
-    // Update seconds
-    res.end();
+router.use((req, res, next) => {
+    console.log("timestamp middleware");
+    next();
 });
+router.get("/", timestampGET);
+router.post("/", timestampPOST);
 
 module.exports = router;
