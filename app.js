a7cb758c3a88859cb42f562cbdfd1d53ee898713
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2023-11-05 14:45:37 +0000

    Add PORT constant

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // const server = require("./public/server/server");
 const { app } = require("./backend/server");
+const PORT = 3000;
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000...");
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
 });
