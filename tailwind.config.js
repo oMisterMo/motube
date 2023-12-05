170e91289067c57f4d1dca70f2683b38ec99899c
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2023-12-05 00:55:39 +0000

    Toggle dark mode manual

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./public/**/*.{js,jsx,ts,tsx,vue,html}"],
+    // Support toggling dark mode manually instead of relying on the operating system preference
+    darkMode: "class",
     theme: {
         extend: {},
     },
